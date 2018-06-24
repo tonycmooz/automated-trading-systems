8b6498ad39bea4b09d33111768150f7d9e863d06
Tony Moozhayil <tonycmooz@gmail.com>
2018-06-24 09:59:15 -0400

    Add files via upload
    This script processes data feeds from the BATS TCP PITCH protocol by timestamp, message type, order ID, indicator, shares, symbol, and price. 
    For more info: http://www.batstrading.com/resources/membership/BATS_PITCH_Specification.pdf

---
+++ process_pitch_data_feeds.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+__author__ = 'tonycmooz'
+from collections import Counter
+
+
+def handle():
+    with open('/Users/tonycmooz/Downloads/pitch_example_data', 'r') as f:
+        cnt = Counter()
+        for i, row in enumerate(f):
+            try:
+                row = row.lstrip('S')
+                timestamp, message_type, order_id, indicator, shares, symbol, price, display = \
+                    row[:8], row[8:9], row[9:21], row[21:22], row[22:28], row[28:34], row[34:44], row[44:45]
+                print timestamp, message_type, order_id, indicator, shares, symbol, price, display
+                # if message_type is not ('B', 's', 'r', 'd', 'H', 'I', 'J', 'R'):
+                #     cnt[symbol] += 1
+            except ValueError:
+                print 'Failed: {}'.format(i)
+
+    # return cnt.most_common(11)[1:]
+
+
+if __name__ == "__main__":
+    handle()
